04ecca0a5cb0306df73e97d1422ba255cf9688a5
Nicholas Dhaeyer <87302600+DhaeyerWolf@users.noreply.github.com>
2023-02-20 17:54:19 +0000

    added a weight system that can be changed

--- onenoteparser.py
+++ onenoteparser.py
@@ -4,9 +4,16 @@ import binascii
 import sys 
 
 if len(sys.argv) == 1:
-    print("Please provide an argument")
+    print("Usage: python3 onenoteparser.py filename.one [weight]")
     sys.exit()
 
+if sys.argv != 2:
+	try:
+		weight = int(sys.argv[2])
+	excpet:
+		print('Error with the weight, switching to defailt weight: 500. The higher the weight the more results will be displayed.')
+else:
+	weight = 500
 # Open the file and read in the contents
 with open(sys.argv[1], 'rb') as f:
 	data = f.read()
@@ -18,10 +25,8 @@ i = 0
 for start in start_indexes:
 	end = data.find(b'\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x00\x00\x00\x00', start + 1)
 	block = data[start:end]
-	if len(block) > 1000 and len(block) < 5000 and not block.count(b'\x00') > 500:
-		#print('- {}'.format(block.hex()))
+	if len(block) > 1000 and len(block) < 5000 and not block.count(b'\x00') > weight:
 		block = block.hex()
-		#print(bytes.fromhex(block).decode('latin-1'))
 		readable_chars = bytes([x for x in bytes.fromhex(block) if x in range(32, 127)]).decode('latin-1')
 		print('string {}: {}'.format(i,readable_chars))
 		i+=1
